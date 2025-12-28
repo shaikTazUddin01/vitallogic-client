a9cf841317fe2ae40f7e9ada7c29a7e8ebc9cba5
shaikTazUddin01 <tazahmedsoft@gmail.com>
2025-12-28 20:21:51 +0600

    set up new project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                bg: "var(--bg)",
+                surface: "var(--surface)",
+                border: "var(--border)",
+
+                primary: "var(--primary)",
+                "primary-text": "var(--primary-text)",
+
+                accent: "var(--accent)",
+
+                text: {
+                    primary: "var(--text-primary)",
+                    secondary: "var(--text-secondary)",
+                },
+            },
+        },
+    },
+};
